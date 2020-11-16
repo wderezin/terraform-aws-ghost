d7aeefd97b8baf943c51891be23fbf67e61601c9
Walter Derezinski <walter@daringway.com>
2020-11-16 12:43:56 -0500

    added append_slash option

--- modules/ghost/main-lambdas.tf
+++ modules/ghost/main-lambdas.tf
@@ -3,11 +3,12 @@ module default-cloudfront-s3-viewer-request-lambda {
   count = local.use_default_request_lambda ? 1 : 0
 
   source               = "daringway/cloudfront-viewer-request-lambda/aws"
-  version              = "1.1.0"
+  version              = "1.2.0"
   tags                 = local.tags
   lambda_name          = "${local.application}-website-viewer_request"
   apex_domain_redirect = true
   index_rewrite        = true
+  append_slash         = true
   ghost_hostname       = local.cms_fqdn
 }
 
