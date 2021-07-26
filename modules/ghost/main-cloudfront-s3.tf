f510baf01124a4da9f83aacb2938e496ab2b5fbd
Walter Derezinski <walter@daringway.com>
2021-07-26 14:45:40 -0400

    adding multi options for cdn origin: live, static, failover

--- modules/ghost/main-cloudfront-s3.tf
+++ modules/ghost/main-cloudfront-s3.tf
@@ -71,7 +71,7 @@ resource aws_cloudfront_distribution www {
       custom_origin_config {
         http_port = 80
         https_port = 443
-        origin_protocol_policy = https-only
+        origin_protocol_policy = "https-only"
         origin_ssl_protocols = ["TLSv1.1", "TLSv1.2"]
       }
 
