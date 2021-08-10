4373737e27ce5c2e68e4dde38bf14333d7667818
Walter Derezinski <walter@daringway.com>
2021-08-10 13:56:37 -0400

    502 error page

--- modules/ghost/main-cloudfront-s3.tf
+++ modules/ghost/main-cloudfront-s3.tf
@@ -29,11 +29,11 @@ resource "aws_cloudfront_distribution" "www" {
 
   //  TOOD Update to work with Live mode
   //  //  permission denied from s3
-  //  custom_error_response {
-  //    error_code         = 403
-  //    response_code      = 404
-  //    response_page_path = "/404/index.html"
-  //  }
+  custom_error_response {
+    error_code         = 502
+    response_code      = 502
+    response_page_path = "/error/502.html"
+  }
   //
   //  custom_error_response {
   //    error_code         = 404
@@ -116,6 +116,45 @@ resource "aws_cloudfront_distribution" "www" {
     connection_timeout  = 10
   }
 
+  // S3
+  origin {
+    domain_name = aws_s3_bucket.web.bucket_regional_domain_name
+    origin_id   = local.static_origin_id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.default.cloudfront_access_identity_path
+    }
+  }
+
+  ordered_cache_behavior {
+    path_pattern = "/error/*"
+    allowed_methods = [
+      "GET",
+      "HEAD",
+      "OPTIONS",
+    ]
+    cached_methods = [
+      "HEAD",
+      "GET"
+    ]
+    target_origin_id = local.static_origin_id
+
+    forwarded_values {
+      query_string = true
+      headers      = ["Origin", "Referer", "User-Agent"]
+
+      //      cookies {
+      //        forward = "all"
+      //      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 60
+    max_ttl                = 60
+    compress               = false
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
 
   ordered_cache_behavior {
     path_pattern = "/ghost/*"
@@ -195,18 +234,6 @@ resource "aws_cloudfront_distribution" "www" {
     }
   }
 
-  dynamic "origin" {
-    for_each = local.enable_static
-    content {
-      domain_name = aws_s3_bucket.web.bucket_regional_domain_name
-      origin_id   = local.static_origin_id
-
-      s3_origin_config {
-        origin_access_identity = aws_cloudfront_origin_access_identity.default.cloudfront_access_identity_path
-      }
-    }
-  }
-
   dynamic "default_cache_behavior" {
     for_each = local.enable_static
     content {
@@ -219,7 +246,7 @@ resource "aws_cloudfront_distribution" "www" {
         "HEAD",
         "GET"
       ]
-      target_origin_id = local.origin_id
+      target_origin_id = local.static_origin_id
 
       forwarded_values {
         query_string = false
