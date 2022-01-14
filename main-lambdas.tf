c159596c67be9a65672c32aa2518b124c70b9b4a
Walter Derezinski <walter@daringway.com>
2022-01-13 23:38:45 -0500

    restoring lambda@edge so it can be deleted in 2 steps

---
+++ main-lambdas.tf
@@ -0,0 +1,13 @@
+
+module "default-cloudfront-s3-viewer-request-lambda" {
+  count = local.use_default_request_lambda ? 1 : 0
+
+  source               = "daringway/cloudfront-viewer-request-lambda/aws"
+  version              = "1.2.3"
+  tags                 = local.tags
+  lambda_name          = "${local.application}-website-viewer_request"
+  apex_domain_redirect = local.enable_root_domain
+  index_rewrite        = local.cdn_mode == "static"
+  append_slash         = true
+  ghost_hostname       = local.cdn_mode == "static" ? local.cms_fqdn : ""
+}
